5040ad86ffaee697ac01513605508e8edf9c255c
Pedinn <pedrosilonliveira05@gmail.com>
2021-08-11 16:36:47 -0300

    enter

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TESTANDO! TESTANDO! TESTANDO!</Text>
+      <Text>TESTANDO! TESTANDO!</Text>
       <StatusBar style="auto" />
     </View>
   );
