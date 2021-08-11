4c4a115cc1e71f02eceadf298a426abddb7be9df
Pedinn <pedrosilonliveira05@gmail.com>
2021-08-11 16:59:44 -0300

    enter

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -4,7 +4,7 @@ import { View, Text, SafeAreaView, StatusBar } from 'react-native'
 export default class TaskList extends Component {
     render() {
         return (
-            <SafeAreaView style={{flex: 1, paddingTop: StatusBar.currentHeight}}>
+            <SafeAreaView style={{flex: 100, paddingTop: StatusBar.currentHeight}}>
                 <View>
                     <Text>TaskList</Text>
                 </View>
