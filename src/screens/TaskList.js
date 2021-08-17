c07f032d132537479a49e8b1f77fd54eb853dad0
Pedinn <pedrosilonliveira05@gmail.com>
2021-08-17 17:28:27 -0300

    enter

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -14,7 +14,7 @@ export default class TaskList extends Component {
                 <View style={styles.container}>
                     <ImageBackground source={todayImage}
                         style={styles.background}>
-                        <View>
+                        <View style={styles.titleBar}>
                             <Text>Hoje</Text>
                             <Text>{today}</Text>
                         </View>
@@ -38,4 +38,9 @@ const styles = StyleSheet.create({
     taskList: {
         flex: 7,
     },  
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+
+    },
 })
