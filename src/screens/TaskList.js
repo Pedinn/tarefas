6b581a4bc3f7577210f0c20d8354806fb3ccefbd
Pedinn <pedrosilonliveira05@gmail.com>
2021-08-19 18:49:53 -0300

    enter

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -14,6 +14,8 @@ import todayImage from '../../assets/imgs/today.jpg'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 
+import Task from '../components/Task'
+
 export default class TaskList extends Component {
     render() {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
@@ -28,9 +30,10 @@ export default class TaskList extends Component {
                             </View>
                     </ImageBackground>
                     <View style={styles.taskList}>
-                        <Text>Tarefa #01</Text>
-                        <Text>Tarefa #02</Text>
-                        <Text>Tarefa #03</Text>
+                        <Task desc="Comprar Livro" estimateAt={new Date()}
+                            doneAt={new Date()} />
+                        <Task desc="Ler livro" estimateAt={new Date()}
+                            doneAt={null} />
                     </View>
                 </View>
             </SafeAreaView>
