a08411257812e3d09c2d679ca361b73a2a02f699
Pedinn <pedrosilonliveira05@gmail.com>
2021-08-19 19:43:48 -0300

    enter

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,57 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { Icon } from 'react-native-vector-icons/FontAwesome'
+
+import commonStyles from '../commonStyles'
 
 export default props => {
 
     return (
-        <View>
-            <Text>{props.desc}</Text>
-            <Text>{props.estimateAt + ""}</Text>
-            <Text>{props.doneAt + ""}</Text>
+        <View style={styles.container}>
+            <View style={styles.checkContainer}>
+                {getCheckView(props.doneAt)}
+            </View>
+            <View>
+                <Text>{props.desc}</Text>
+                <Text>{props.estimateAt + ""}</Text>
+            </View>
+                    
         </View>
     )
 }
+
+function getCheckView(doneAt){
+    if(doneAt != null){
+        return (
+            <View>
+                <Text>Concluida</Text>
+            </View>
+        )
+    }else {
+        return (
+            <View style={styles.peding}></View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        borderColor: '#AAA',
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+    checkContainer: {
+        width: "20%",
+        alignItems: 'center',
+        justifyContent: 'center,'
+    },
+    peding: {
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        borderWidth: 1,
+        borderColor: '#555'
+    },
+})
