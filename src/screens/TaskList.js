0ac9b3f797793f613c7a1857104d5b4a2a775c03
Pedinn <pedrosilonliveira05@gmail.com>
2021-09-02 15:38:02 -0300

    enter

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -25,7 +25,7 @@ import AddTasks from './AddTasks'
 export default class TaskList extends Component {
     state = {
         showDoneTasks: true,
-        showAddTasks: true,
+        showAddTasks: false,
         visibleTasks: [],
         tasks: [{
             id: Math.random(),
@@ -97,6 +97,11 @@ export default class TaskList extends Component {
                             keyExtractor={item => `${item.id}`}
                             renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} />}/>
                     </View>
+                    <TouchableOpacity style={styles.addButton}
+                        activeOpacity={0.7}
+                        onPress={() => this.setState({ showAddTasks: true })}>
+                        <Icon name ="plus" size ={20} color={commonStyles.colors.secodary}/>
+                    </TouchableOpacity>
                 </View>
             </SafeAreaView>
         )
@@ -136,5 +141,16 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
         justifyContent: 'flex-end',
         marginTop: Platform.OS === 'ios' ? 40 : 10
+     },
+     addButton: {
+         position: 'absolute',
+         right: 30,
+         bottom: 30,
+         width: 50,
+         height: 50,
+         borderRadius: 25,
+         backgroundColor: commonStyles.colors.today,
+         justifyContent: 'center',
+         alignItems: 'center',
      },
 })
